8b7d2a0eba7e9e072d97eeccd89413bb547fafa6
Hadrien Patte <hadrien.patte@datadoghq.com>
2026-04-23 16:28:55 +0200

    docs: Extend Azure IPAM documentation
    Update Azure IPAM documentation to add more details around:
    * required IAM permissions
    * authentication modes (in particular the
      `--azure-user-assigned-identity-id` flag)
    * troubleshooting common failure modes
    
    The goal of this PR is to address the points raised in #16164.
    
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- operator/pkg/ipam/azure.go
+++ operator/pkg/ipam/azure.go
@@ -49,8 +49,8 @@ var azureDefaultConfig = AzureConfig{
 
 func (cfg AzureConfig) Flags(flags *pflag.FlagSet) {
 	flags.String(operatorOption.AzureSubscriptionID, azureDefaultConfig.AzureSubscriptionID, "Subscription ID to access Azure API")
-	flags.String(operatorOption.AzureResourceGroup, azureDefaultConfig.AzureResourceGroup, "Resource group to use for Azure IPAM")
-	flags.String(operatorOption.AzureUserAssignedIdentityID, azureDefaultConfig.AzureUserAssignedIdentityID, "ID of the user assigned identity used to auth with the Azure API")
+	flags.String(operatorOption.AzureResourceGroup, azureDefaultConfig.AzureResourceGroup, "Resource group containing the cluster nodes (defaults to the operator's own resource group via Azure IMS)")
+	flags.String(operatorOption.AzureUserAssignedIdentityID, azureDefaultConfig.AzureUserAssignedIdentityID, "Client ID (UUID) of the user-assigned managed identity used to auth with the Azure API")
 	flags.Bool(operatorOption.AzureUsePrimaryAddress, azureDefaultConfig.AzureUsePrimaryAddress, "Use Azure IP address from interface's primary IPConfigurations")
 }
 
